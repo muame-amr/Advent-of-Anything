bff6b93da90dae2f988bc8d5577e4cdc090bede5
Muhammad <42296446+muame-amr@users.noreply.github.com>
2024-12-03 15:18:12 +0800

    AOC Day2

---
+++ 2024/Advent-of-Code-2024/Day2/Part2/solution.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("input.in")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	total := 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		a := strings.Split(scanner.Text(), " ")
+		nums := make([]int, len(a))
+		for i, v := range a {
+			nums[i], err = strconv.Atoi(v)
+			if err != nil {
+				fmt.Println("Conversion error !")
+				return
+			}
+		}
+		if nums[len(nums)-1] < nums[0] {
+			nums = reverse(nums)
+		}
+		// total += safeCheck(nums)
+		total += reallySafeCheck(nums)
+	}
+	fmt.Println(total)
+}
+
+func reverse(nums []int) []int {
+	for i, j := 0, len(nums)-1; i < j; i, j = i+1, j-1 {
+		nums[i], nums[j] = nums[j], nums[i]
+	}
+	return nums
+}
+
+func safeCheck(nums []int) int {
+	for i := 1; i < len(nums); i++ {
+		if nums[i]-nums[i-1] > 3 || nums[i]-nums[i-1] < 1 {
+			return 0
+		}
+	}
+	return 1
+}
+
+func reallySafeCheck(nums []int) int {
+	if safeCheck(nums) == 0 {
+		for i := 0; i < len(nums); i++ {
+			s := make([]int, len(nums))
+			copy(s, nums)
+			if safeCheck(append(s[:i], s[i+1:]...)) == 1 {
+				return 1
+			}
+		}
+		return 0
+	}
+	return 1
+}
