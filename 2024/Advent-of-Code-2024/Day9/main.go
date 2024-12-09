847c817c1960a4ac0cec75ac23c7cf7daa9fefa3
Muhammad Amiruddin <42296446+muame-amr@users.noreply.github.com>
2024-12-10 03:46:46 +0800

    Day9 P1

--- 2024/Advent-of-Code-2024/Day9/main.go
+++ 2024/Advent-of-Code-2024/Day9/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	content, _ := os.ReadFile("input.in")
+	fileContent := strings.TrimSpace(string(content))
+	solveA(fileContent)
+	// solveB(fileContent)
+	elapsed := time.Since(start)
+	fmt.Printf("Execution time: %s\n", elapsed)
+}
+
+// result:
+// 6370402949053
+// 6370402949053
+// test: 523702
--- 2024/Advent-of-Code-2024/Day9/part1.go
+++ 2024/Advent-of-Code-2024/Day9/part1.go
@@ -1,1 +1,49 @@
 package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func solveA(fileContent string) {
+	blocks := []rune{}
+	fileId := 0
+	idx := 0
+	freeQueue := []int{}
+	fragStack := []int{}
+
+	for pos, char := range strings.Split(fileContent, "") {
+		diskMap, _ := strconv.Atoi(char)
+		if pos%2 == 0 {
+			for i := 0; i < diskMap; i++ {
+				blocks = append(blocks, rune('0'+fileId))
+				fragStack = append(fragStack, idx)
+				idx++
+			}
+			fileId++
+		} else {
+			for i := 0; i < diskMap; i++ {
+				blocks = append(blocks, rune('.'))
+				freeQueue = append(freeQueue, idx)
+				idx++
+			}
+		}
+	}
+
+	lastIdx := len(fragStack) - 1
+	for freeQueue[0] <= fragStack[lastIdx] {
+		blocks[freeQueue[0]] = blocks[fragStack[lastIdx]] // swap
+		blocks[fragStack[lastIdx]] = '.'
+		freeQueue = freeQueue[1:] // dequeue
+		lastIdx--                 // pop stack
+	}
+
+	total := 0
+	blockId := 0
+	for blocks[blockId] != '.' {
+		total += blockId * int(blocks[blockId]-'0')
+		blockId++
+	}
+	fmt.Println(total)
+}
